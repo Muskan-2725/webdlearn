268a0011edbee7a7bd367ae8cea81a1a8f5a7907
muskan garg <muskangarg@muskans-MacBook-Air-2.local>
2025-03-26 11:00:55 +0530

    functions basics

--- learn.js
+++ learn.js
@@ -317,3 +317,98 @@ console.table([id,name,roll,clg,add]);*/
 // console.log(Object.values(tinderuser))
 // console.log(Object.entries(tinderuser))
 // console.log(tinderuser.hasOwnProperty("isloggedin"))
+
+//+++++++++++object destructure+++++++++++
+// const course={
+//     name:"b.tech",
+//     clg:"nsut",
+//     courseinstructor:"6576ggj",
+// }
+//////first method:course.id
+//////another: de-structure
+// const {courseinstructor}=course
+// console.log(courseinstructor)
+// const {courseinstructor:instructor}=course
+// console.log(instructor)
+// //api:json format
+// //as object
+// {
+//     "name":"muskan",
+//     "batch":"cse2",
+// }
+// //or as array
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
+
+
+//-----------functions---------
+// function letter(){
+//     console.log('M')
+//     console.log('u')
+//     console.log('s')
+//     console.log('k')
+//     console.log('a')
+//     console.log('n')
+// }
+// // writting just letter would result in nothing it just states refrence rather
+// letter()
+
+// //for adding two numbers
+// function addtwonumbers(number1,number2){
+//     console.log(number1+number2)
+// }
+// addtwonumbers(4,5) // we need to pass on parameters in the function as NaN
+// const result=addtwonumbers(4,5)
+// console.log("result of addtwonumbers using console:",result)
+// //can also use return statement but that won't print unless function is printed
+// function AddTwoNumbers(number1, number2){
+//     let result=number1+number2
+//     return result
+//     //or simply return number1+number2
+// }
+// console.log(AddTwoNumbers(4,5))
+// const result2=AddTwoNumbers(4,5)
+// console.log("result of AddTwoNumbers using return:",result2)
+
+// function isloggedin(name="sam"){//we can even have predefined value for the argument
+//     // if(name===undefined){//or!name i.e parameter not passed 
+//     //     console.log("please enter the name ")
+//     //     return 
+//     // }
+//     return `${name} is logged in`
+// }
+// console.log(isloggedin("muskan"))
+// console.log(isloggedin())
+
+
+//+++++++++++++ arrays and objects in functions ++++++++++++
+// function calculatecartprice(num1){
+//     return num1
+// }
+// console.log(calculatecartprice(399))
+// //what if we have multiple values the above fucntion would only take first argument and ignore the rest 
+// function CalculateCartPrice(...num1){// ... represent rest operator
+//     return num1
+// }
+// console.log(CalculateCartPrice(200,300,500,1000))//returns in form of an array
+
+// const user={
+//     name:"muskan",
+//     price:399
+// }
+// function handleobject(anyobj){
+//     return `username is ${anyobj.name} and price is ${anyobj.price}`
+// }
+// console.log(handleobject(user))
+// console.log(handleobject({name:"mukul",price:500}))
+
+// const arr=[200,500,400,2000]
+// function handlearr(anyarr){
+//     return anyarr[2]
+// }
+// console.log(handlearr(arr))
+// console.log(handlearr([300,800,100]))
