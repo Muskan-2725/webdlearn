5cc88d51959756d66ddea40b0c2641ec446c6d5b
muskan garg <muskangarg@muskans-MacBook-Air-2.local>
2025-05-28 20:29:33 +0530

    added new folder

---
+++ webdlearn/js_basics/datatypes.js
@@ -0,0 +1,39 @@
+//"use strict"; //treats js as newer version
+
+//======variables and constant=======
+console.log("hello");
+const id=12974
+let name="muskan"
+var roll="2023UCS1635"
+clg="nsit"
+let add;
+name="mukul"
+console.log(id);
+console.log(name);
+console.table([id,name,roll,clg,add]);
+
+
+
+//=======datatypes: number, string, boolean, null(stand alone)(type0f:object),undefined=========
+let str=undefined
+let strnumber=Number(str)
+console.log(typeof str)
+console.log(typeof strnumber)
+console.log(strnumber)
+// str=>33 ; result==33
+// str=>33abc ; result==NaN
+// str=>null ;result==0
+// str=>true/false ;result==1/0
+// str=>undefined ;result==NaN
+ let num=23
+ let numstr=String(num)
+ console.log(numstr)
+
+ let loggedin=-33
+ let booleanloggedin=Boolean(loggedin)
+ console.log(typeof booleanloggedin)
+ console.log(booleanloggedin)
+ //loggedin=>1/0 ;result==true/false
+ //loggedin=>"muskan" ;result==true
+ //loggedin=>"" ;result==false
+ //loggedin=>somenumber ;result==true
