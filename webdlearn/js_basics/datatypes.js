dff95410c9c8426c21b405b8905ae4e385721b0d
muskan garg <muskangarg@muskans-MacBook-Air-2.local>
2025-05-28 20:40:55 +0530

    updated datatypes

--- webdlearn/js_basics/datatypes.js
+++ webdlearn/js_basics/datatypes.js
@@ -3,14 +3,14 @@
 //======variables and constant=======
 console.log("hello");
 const id=12974
-let name="muskan"
+let myname="muskan"
 var roll="2023UCS1635"
 clg="nsit"
 let add;
-name="mukul"
+myname="mukul"
 console.log(id);
-console.log(name);
-console.table([id,name,roll,clg,add]);
+console.log(myname);
+console.table([id,myname,roll,clg,add]);
 
 
 
@@ -37,3 +37,39 @@ console.log(strnumber)
  //loggedin=>"muskan" ;result==true
  //loggedin=>"" ;result==false
  //loggedin=>somenumber ;result==true
+
+
+ //**********summary**********
+// //primitive datatypes
+// 7 types : Number, String , Boolean, null,undefined, Symbol(unique),BigInt
+const myid=Symbol('123')
+const idsaves=Symbol('123')
+console.log(id===idsaves)//returns false
+const bignum=2874691485035n//BigInt :apply n at end 
+//refrence (non-primitive)
+Array, Object, Function
+const heroes=["shaktiman","nagraj"]
+let myobj={
+    name:"muskan",
+    roll:23,
+}
+const myfunc= function(){
+    console.log("hello world");
+}
+
+//++++++++memory++++++
+// // Stack(primitive),heap (non-prmitive)
+let name="muskan"
+let anothername=name
+anothername="mukul"
+console.log(anothername)
+console.log(name)
+
+let detail1={
+    id:"12434",
+    email:"muskan@gmail.com"
+}
+let detail2=detail1
+detail2.email="garg@gmail"
+console.log(detail1.email)
+console.log(detail2.email)
